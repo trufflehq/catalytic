1479ca865aa295d494a19385ae629897b390b180
Carter Himmel <me@fyko.net>
2023-08-01 18:50:53 -0600

    test: delete example

--- catalytic_table_to_struct/example/src/non_compiling_code/non_existing_table.rs
+++
@@ -1,8 +0,0 @@
-use catalytic::scylla;
-use catalytic_macro::query;
-
-fn main() -> Result<(), scylla::frame::value::SerializeValuesError> {
-    query!("select * from idontexist");
-
-    Ok(())
-}
