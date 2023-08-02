3aeed28d8634a795b55e74920e17c8aa3681c74d
Carter Himmel <me@fyko.net>
2023-08-01 19:13:55 -0600

    fix: build error

--- catalytic_query_parser/src/extract_query_metadata.rs
+++ catalytic_query_parser/src/extract_query_metadata.rs
@@ -229,7 +229,10 @@ fn add_random_value(
         ColumnType::Double => into!(f64::MAX),
         ColumnType::Uuid => {
             into!(uuid::Uuid::parse_str("3866a82f-f37c-446c-8838-fb6686c3acf2").unwrap())
-        }
+        },
+        ColumnType::TimeUuid => {
+            into!(uuid::Uuid::parse_str("3866a82f-f37c-446c-8838-fb6686c3acf2").unwrap())
+        },
         ColumnType::Custom(_) => {
             panic!("https://github.com/scylladb/scylla-rust-driver/issues/104")
         }
